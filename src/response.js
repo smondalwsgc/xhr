3d698a428cd8e7200085fdbfc762c82060ec454f
sandip mondal <smondal2@wsgc.com>
2019-08-01 20:51:32 +0530

    Handle 422 response

--- src/response.js
+++ src/response.js
@@ -1,12 +1,14 @@
 import Headers from './headers'
+import {HttpStatus} from './utils'
 const Response = function(bodyInit, response, options){
+  var status = new HttpStatus(response.status)
   this.type = 'default'
   this.status = response.status === undefined ? 200 : response.status
-  this.ok = this.status >= 200 && this.status < 300
+  this.ok = status.success
   this.statusText = 'statusText' in response ? response.statusText : 'OK'
   this.headers = new Headers(options.headers)
   this.url = options.url || ''
-  if(this.ok)
+  if(this.ok || status.client_error)
     this.data = JSON.parse(bodyInit)
 
 }
--- src/utils.js
+++ src/utils.js
@@ -11,7 +11,15 @@ let normalize_method = function(method){
     return ''
 }
 
+const HttpStatus = function(status){
+  this.success = status >=200 && status < 300
+  this.redirection = status >=300 && status < 400
+  this.client_error = status >=400 && status < 500
+  this.server_error = status >=500 && status < 600
+}
+
 module.exports = {
   merge_options: merge_options,
-  normalize_method: normalize_method
+  normalize_method: normalize_method,
+  HttpStatus: HttpStatus
 }
